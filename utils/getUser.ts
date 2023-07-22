5f55d6f21fb6c588da944ad822e2497e247e5b63
Rishab Sharma <46566471+Rishab1207@users.noreply.github.com>
2023-07-22 11:01:14 +0530

    removed hard coded url

--- utils/getUser.ts
+++ utils/getUser.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
 import { cache } from "react";
 
+const URL = 'https://64baaf385e0670a501d6887f.mockapi.io/users'
+
 export const getUsers = cache(async () => {
     const users = (
-        await axios.get("https://64baaf385e0670a501d6887f.mockapi.io/users")
+        await axios.get(URL)
     ).data;
 
     return users;
 });
 
 export const fetchUsers = async () => {
-    const users = (await fetch("https://64baaf385e0670a501d6887f.mockapi.io/users")).json();
+    const users = (await fetch(URL)).json();
 
     return users;
 };
